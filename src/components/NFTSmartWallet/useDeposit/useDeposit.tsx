c630c14916763e19c65a477315727cf38641eb1a
Eric Juta <ericjohnjuta@gmail.com>
2020-10-28 00:58:28 +0000

    Purge index page

--- src/components/NFTSmartWallet/useDeposit/useDeposit.tsx
+++ src/components/NFTSmartWallet/useDeposit/useDeposit.tsx
@@ -4,7 +4,6 @@ import { NftSmartWalletMvp } from "../../../types";
 
 export const useDeposit = () => {
   const { nftSmartWalletMvp } = useContext(Web3Context);
-  console.log(nftSmartWalletMvp);
   const mvp = nftSmartWalletMvp?.[0];
   return { depositNFT: mvp?.depositNFT, depositToken: mvp?.depositToken };
 };
